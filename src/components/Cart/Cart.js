509e8eccac363368ea27034ed229c81325df4091
jakiasultana2022 <rokeyasbd@gmail.com>
2022-07-01 21:13:38 +0600

    ema-john-shop

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -3,7 +3,8 @@ import './Cart.css';
 
 const Cart = (props) => {
     const { cart } = props;
-    // console.log(cart);
+    console.log(props.children);
+
     let total = 0;
     let shipping = 0;
     let quantity = 0;
@@ -22,6 +23,8 @@ const Cart = (props) => {
             <p>Total Shipping: ${shipping}</p>
             <p>Tax: {tax}</p>
             <h5>Grand Total: {grandTotal.toFixed(2)}</h5>
+
+            {props.children}
         </div>
     );
 };
